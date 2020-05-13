814064255a1cf83407e4d369ae254d1a60b014d8
unknown <christian_kfoury558@hotmail.ca>
2020-05-13 01:23:01 -0400

    Creating a startMenu method that welcome the user and outputs the rules

--- Hangman.java
+++ Hangman.java
@@ -20,7 +20,7 @@ public class Hangman {
         File inFile = new File("C:/Users/Chris/chris/HangmanDictionary.txt");
         // Reading the Dictionnary File with the scanner class
         Scanner scFile = new Scanner(inFile);
-        
+
         // Declaring and initializing some values
         /* This value beginning index and endIndex  is for changing the hidden letter of 
            the hidden word with the letter inputted by the user if it is valid */
@@ -36,5 +36,55 @@ public class Hangman {
         String letterAlreadyGuessed = "";
         // This value will be used the store the answer of the user if the user wants to play again
         String answer = "";
+
+        // Calling the startMenu() method
+        startMenu();
+        
+        // A word that will be selected out of the dictionnary text file
+        String word = scFile.next().toLowerCase();
+        // Length of word
+        int numberOfLetters = word.length();
+        // Output how many letter does the word contain
+        System.out.println("This word contains " + numberOfLetters + " letters");
+        // A hidden word that is disguised by "?" to play as the word
+        String hiddenWord = word.replaceAll(".", "?");
+        // Output the hidden word
+        System.out.println(hiddenWord);
+    }
+    /**
+     * This method welcome the user and outputs the rules, 
+     * then this method will ask the user if they want to start the game by entering 1 or quit game by entering 0
+     */
+    public static void startMenu(){
+        // Welcome output
+        System.out.println("Welcome to hangman!\n");
+        // Output the rules
+        System.out.println("These are the rules:");
+        System.out.println("- You have 6 guesses at each word");
+        System.out.println("- You can guess a letter or a word");
+        System.out.println("- +1 points if a letter is guessed ");
+        System.out.println("- -1 points if letter guessed is wrong");
+        System.out.println("- +2 points if a word is guessed");
+        System.out.println("- -2 points if word guessed is wrong\n");
+        
+        System.out.println("0. Quit Game\n1. Start Game\n Enter your choice: ");
+        Scanner input = new Scanner(System.in);
+        int choice = input.nextInt();
+        switch (choice) 
+        {
+        case 0:
+            System.out.println("Thanks for playing the game");
+            System.exit(0);
+            break;
+    
+        case 1:
+            System.out.println("Game Initalized");
+        default:
+            System.out.println("Error: unnexpected value");
+            System.out.println("0. Quit Game\n1. Start Game\n Enter your choice: ");
+            startMenu();
+            break;
+        }
     }
 }
+
